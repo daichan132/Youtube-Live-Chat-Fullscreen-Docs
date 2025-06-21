3ad0ddbecae89fd3a8dc9c0fd570ef70cbaa531c
daichan133322 <wakadai0319@gmail.com>
2025-06-21 18:19:37 +0900

    プラグインから未使用の reactRouterDevTools を削除

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { reactRouterDevTools } from "react-router-devtools";
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/Youtube-Live-Chat-Fullscreen-Docs/' : undefined,
-  plugins: [tailwindcss(), reactRouterDevTools(), reactRouter(), tsconfigPaths()]
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
 }));
